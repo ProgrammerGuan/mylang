a6f347a5e73a83f68099ad966f36142520e724ff
guancheng <guancheng@toydea.com>
2020-02-13 19:12:01 +0900

    コメントを作成した。

--- MyLang/SpaceSeparatedTokenizer.cs
+++ MyLang/SpaceSeparatedTokenizer.cs
@@ -45,44 +45,49 @@ namespace MyLang
                         //　// と /*　どっちらもない
                         if(now_character[i+1]!="/"&&now_character[i+1]!="*")
                             dummy.Add(new Token(TokenType.Slash, "/"));
+                        //　　//の時
                         else if (now_character[i + 1] == "/")
                         {
-                            //  //の時に　後ろ全部無視する
+                            //後ろ全部無視する
                             dummy.Add(new Token(TokenType.Terminate, "[EOF]"));
                             return dummy;
                         }
-                        else  // /* の時に
+                        // /* の時に
+                        else
                         {
                             i += 2;
                             while (true)
                             {
+                                //　もし今の字は "/" それて次の言葉は "*" の時に　コメントが終了
                                 if (now_character[i] == "/" && now_character[i-1] == "*")
                                     break;
-                                else
+                                else  //iは今の字のインデックスです、ずっと後ろへ進む
                                     i++;
                             }
-
                         }
                         break;
                     default:
+                        // Symbolと数字の時
                         int num = 0;
                          is_number = int.TryParse(now_character[i], out num);
-                        if(!is_number)//not number
+                        if(!is_number)//Symbolの時
                         {
+                            //もし今の字はSymbolれば　どんどん次の言葉をチェックします
                             while (i < now_character.Count && Char.IsLetter(now_character[i].ToCharArray()[0]))
                             {
                                 single_word.Add(now_character[i++]);
                                 if (i == now_character.Count)
                                     break;
                             }
+                            //その先にインデックスをプラスしたから、今の字をチェックしたいければ、インデックスをマイナスします
                             if (i < now_character.Count - 1)
                                 i -= 1;
                             dummy.Add(new Token(TokenType.Symbol, string.Join("", single_word.ToArray())));
                             single_word.Clear();
-                            //dummy.Add(new Token(TokenType.Symbol, now_character[i]));
                         }
-                        else // is number
+                        else // 数字の時
                         {
+                            //同じ方法をしましたけと、TokenTypeに注意してください。
                             while (i<now_character.Count && int.TryParse(now_character[i], out num))
                             {
                                 single_word.Add(now_character[i++]);
