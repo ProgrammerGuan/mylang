fdfb39dae3e82e8f46f164cd8920a485efcbb896
guancheng <guancheng@toydea.com>
2020-02-14 20:15:27 +0900

    Temp saving

--- MyLang/Parser.cs
+++ MyLang/Parser.cs
@@ -45,18 +45,72 @@ namespace MyLang
         public Ast.Ast Parse(IList<Token> tokens)
         {
             tokens_ = tokens;
+            tokens_.RemoveAt(tokens_.Count - 1);
             pos_ = 0;
 
             // TODO: 仮のダミー実装
             
-            var lhs = new Ast.Number(1);
-            var rhs = new Ast.Number(2);
-            var ast = new Ast.BinOp(Ast.BinOpType.Add, lhs, rhs);
+            //var lhs = new Ast.Number(1);
+            //var rhs = new Ast.Number(2);
+            //var ast = new Ast.BinOp(Ast.BinOpType.Add, lhs, rhs);
 
-            return ast;
+            return Exp1();
         }
         
-        
+        private Ast.Exp Exp1()
+        {
+            var lhs = Exp2();
+            if (lhs == null)
+                return null;
+            var token = currentToken();
+            if (token.Type == TokenType.Plus || token.Type == TokenType.Minus)
+            {
+                var binOpType = BinOpMap[token.Type];
+                progress();
+                var rhs = Exp1();
+                var ast = new Ast.BinOp(binOpType, lhs, rhs);
+                return ast;
+            }
+            else
+            {
+                return lhs;
+            }
+            
+        }
+
+        private Ast.Exp Exp2()
+        {
+            var lhs = Exp_Value();
+            if (lhs == null)
+                return null;
+            if (pos_ == tokens_.Count - 1)
+                return lhs;
+            progress();
+            var token = currentToken();
+            if (token.Type == TokenType.Star || token.Type == TokenType.Slash)
+            {
+                var binOpType = BinOpMap[token.Type];
+                progress();
+                var rhs = Exp2();
+                var ast = new Ast.BinOp(binOpType, lhs, rhs);
+                return ast;
+            }
+            else
+            {
+                return lhs;
+            }
+        }
+
+        private Ast.Exp Exp_Value()
+        {
+            var token = currentToken();
+            if (token.IsNumber)
+                return new Ast.Number(Convert.ToSingle(token.Text));
+            else if (token.IsSymbol)
+                return new Ast.Symbol(token.Text);
+            else throw new Exception(string.Format("Invalid input {0}", token.Text));
+
+        }
 
     }
 }
