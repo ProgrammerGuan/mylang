01de17c8f85960b04c725af9b458708425bc7fcb
guancheng <guancheng@toydea.com>
2020-02-17 16:19:32 +0900

    Interpreter 実装した。

--- MyLang/Interpreter.cs
+++ MyLang/Interpreter.cs
@@ -14,7 +14,37 @@ namespace MyLang
         public float Run(Ast.Ast ast)
         {
             // TODO: 仮のダミー実装
-            return 0;
+            if (ast is Ast.Exp exp)
+                return Answer(exp);
+            else
+                return 0;
+        }
+
+        private float Answer(Ast.Exp exp)
+        {
+            if (exp is Ast.BinOp binOp)
+            {
+                var left_answer = Answer(binOp.Lhs);
+                var right_answer = Answer(binOp.Rhs);
+                switch (binOp.Operator)
+                {
+                    case BinOpType.Add:
+                        return left_answer + right_answer;
+                    case BinOpType.Sub:
+                        return left_answer - right_answer;
+                    case BinOpType.Multiply:
+                        return left_answer * right_answer;
+                    case BinOpType.Divide:
+                        return left_answer / right_answer;
+                    default:
+                        return 0;
+                }
+            }
+            else if (exp is Ast.Number ast_num)
+            {
+                return ast_num.Value;
+            }
+            else return 0;
         }
 
     }
