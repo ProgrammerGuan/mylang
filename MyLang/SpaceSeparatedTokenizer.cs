01363ede420ec6ae0413e8db9da966cd8400e50c
guancheng <guancheng@toydea.com>
2020-02-25 18:47:42 +0900

    time up

--- MyLang/SpaceSeparatedTokenizer.cs
+++ MyLang/SpaceSeparatedTokenizer.cs
@@ -13,6 +13,12 @@ namespace MyLang
     /// </summary>
     class SpaceSeparatedTokenizer : ITokenizer
     {
+        private readonly Regex space = new Regex(@"/s+");
+        private readonly Regex number = new Regex(@"/d+");
+        private readonly Regex single_word = new Regex(@"[a-zA-Z][a-zA-Z_0-9]*");
+        private readonly Regex Operator = new Regex(@"[+-*/]");
+
+
         public SpaceSeparatedTokenizer()
         {
 
@@ -20,147 +26,17 @@ namespace MyLang
 
         public IList<Token> Tokenize(string src)
         {
-            var now_character = new List<string>();
-            //単語の確認時is_Single_Wordを使う
-            bool is_Single_Word = false;
-            foreach (char s in src)
-            {
-                if (s == ' ')
-                {
-                    is_Single_Word = true;
-                    continue;
-                }// これら記号は全部単語の終了
-                else if (s == '=' || s=='+' || s=='-' || s=='*' || s=='/' || s=='{') is_Single_Word = true;
-                if (is_Single_Word)
-                {
-                    is_Single_Word = false;
-                    now_character.Add(" ");
-                }
-                if (s.ToString() == ";") now_character.Add(" ");
-                now_character.Add(s.ToString());
-                if (s.ToString() == "=" || s.ToString() == "+" || s.ToString() == "-" || s.ToString() == "*" || s.ToString() == "/" || s.ToString()=="{") now_character.Add(" ");
-            }
-
+            var codes = space.Split(src);
             // TODO: 仮のダミー実装
             var dummy = new List<Token>();
-            bool is_number = false;
-            var single_word = new List<string>();
-            for (int i = 0; i <now_character.Count; i++)
-            {
-                switch (now_character[i])
-                {
-                    case "+":
-                        dummy.Add(new Token(TokenType.Plus, "+"));
-                        break;
-                    case "-":
-                        dummy.Add(new Token(TokenType.Minus, "-"));
-                        break;
-                    case "*":
-                        dummy.Add(new Token(TokenType.Star, "*"));
-                        break;
-                    case "/":
-                        //　// と /*　どっちらもない
-                        if (i + 1 >= now_character.Count)
-                        {
-                            dummy.Add(new Token(TokenType.Slash, "/"));
-                            dummy.Add(new Token(TokenType.Terminate, "[EOF]"));
-                            return dummy;
-                        }
-                        if (now_character[i+1]!="/"&&now_character[i+1]!="*")
-                            dummy.Add(new Token(TokenType.Slash, "/"));
-                        //　　//の時
-                        else if (now_character[i + 1] == "/")
-                        {
-                            //後ろ全部無視する
-                            dummy.Add(new Token(TokenType.Terminate, "[EOF]"));
-                            return dummy;
-                        }
-                        // /* の時に
-                        else
-                        {
-                            i += 2;
-                            while (true)
-                            {
-                                //　もし今の字は "/" それて次の言葉は "*" の時に　コメントが終了
-                                if (now_character[i] == "/" && now_character[i-1] == "*")
-                                    break;
-                                else  //iは今の字のインデックスです、ずっと後ろへ進む
-                                    i++;
-                            }
-                        }
-                        break;
-                    case "=":
-                        dummy.Add(new Token(TokenType.Equal, "="));
-                        break;
-                    case ";":
-                        dummy.Add(new Token(TokenType.End, ";"));
-                        break;
-                    case "{":
-                        dummy.Add(new Token(TokenType.LeftBlock, "{"));
-                        break;
-                    case "}":
-                        dummy.Add(new Token(TokenType.RightBlock, "}"));
-                        break;
-                    default:
-                        // Symbolと数字の時
-                        int num = 0;
-                         is_number = int.TryParse(now_character[i], out num);
-                        if(!is_number)//Symbolの時
-                        {
-                            //もし今の字はSymbolれば　どんどん次の言葉をチェックします
-                            if (now_character[i] == " ") break;
-                            while (i < now_character.Count )
-                            {
-                                single_word.Add(now_character[i++]);
-                                if (i == now_character.Count || now_character[i]==" ")
-                                    break;
-                            }
-                            //その先にインデックスをプラスしたから、今の字をチェックしたいければ、インデックスをマイナスします
-                            if (single_word.Count > 0)
-                            {
-                                switch (string.Join("", single_word.ToArray()))
-                                {
-                                    //キーワードを確認する
-                                    case "let":
-                                    case "Let":
-                                        dummy.Add(new Token(TokenType.Let, "Let"));
-                                        break;
-                                    case "print":
-                                        dummy.Add(new Token(TokenType.Print, "Print"));
-                                        break;
-                                    case "function":
-                                        dummy.Add(new Token(TokenType.Function, "Function"));
-                                        break;
-                                    case "return":
-                                        dummy.Add(new Token(TokenType.Return, "Return"));
-                                        break;
-                                    default:
-                                        dummy.Add(new Token(TokenType.Symbol, string.Join("", single_word.ToArray())));
-                                        break;
-                                }
-                            }
-                            single_word.Clear();
-                        }
-                        else // 数字の時
-                        {
-                            //同じ方法をしましたけと、TokenTypeに注意してください。
-                            while (i<now_character.Count && int.TryParse(now_character[i], out num))
-                            {
-                                single_word.Add(now_character[i++]);
-                                if (i == now_character.Count)
-                                    break;
-                            }
-                            if(i<now_character.Count-1)
-                                i -= 1;
-                            dummy.Add(new Token(TokenType.Number, string.Join("",single_word.ToArray())));
-                            single_word.Clear(); 
-                        }
-                        break;
-                }
-            }
             
-            dummy.Add(new Token(TokenType.Terminate, "[EOF]"));
+            dummy.Concat(new IEnumerable<Token>(TokenType.Terminate, "[EOF]"));
             return dummy;
+        }
+
+        public Token GetToken(string str)
+        {
+
         }
 
     }
