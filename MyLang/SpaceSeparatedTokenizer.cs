597f8f52f192fdbb3a0fd93d755c600a8efa7e04
guancheng <guancheng@toydea.com>
2020-02-28 11:37:02 +0900

    正規表現を完成した。

--- MyLang/SpaceSeparatedTokenizer.cs
+++ MyLang/SpaceSeparatedTokenizer.cs
@@ -3,16 +3,26 @@ using System.Collections.Generic;
 using System.Text;
 using System.Linq;
 using System.Text.RegularExpressions;
-
 namespace MyLang
 {
+    public static class StringArrayExtension
+    {
+        public static List<string> AllToList(this System.Collections.Generic.IEnumerable<string> words)
+        {
+            var output = new List<string>();
+            foreach (var s in words)
+                if(!new Regex(@"\s+").IsMatch(s) && s!="") output.Add(s);
+            return output;
+        }
+    }
     /// <summary>
     /// 単純なトークナイザ
     /// 
     /// トークンは、必ず一つ以上のスペースで区切られている必要がある
     /// </summary>
-    class SpaceSeparatedTokenizer : ITokenizer
-    {
+    class SpaceSeparatedTokenizer : ITokenizer { 
+
+        private readonly Regex split_word = new Regex(@"(\s+|\+|\-|\*|\/|=|;|\(|\)|\{|\})");
         private readonly Regex space = new Regex(@"\s+");
         private readonly Regex number = new Regex(@"\d+");
         private readonly Regex single_word = new Regex(@"[a-zA-Z][a-zA-Z_0-9]*");
@@ -25,36 +35,44 @@ namespace MyLang
 
         public IList<Token> Tokenize(string src)
         {
-
-            var space_cut = space.Split(src);
-            var codes = new List<string>();
-            foreach(string word in space_cut)
-            {
-                if (Operator.IsMatch(word))
-                {
-                    var Op = Operator.Match(word);
-                    var nums = Operator.Split(word);
-                    if (nums[0] != "") codes.Add(nums[0]);
-                    codes.Add(Op.ToString());
-                    if (nums[1] != "") codes.Add(nums[1]);
-                }
-                else if (KeySymbol.IsMatch(word))
-                {
-                    var sym = KeySymbol.Match(word).ToString();
-                    var num = KeySymbol.Split(word);
-                    if (num[0] != "") codes.Add(num[0]);
-                    codes.Add(sym);
-                    if (num[1] != "") codes.Add(num[1]);
-                }
-                else if(word!="") codes.Add(word);
-            }
+            var codes = split_word.Split(src).AllToList();
             // TODO: 仮のダミー実装
-            return codes.Select(c => GetToken(c)).Concat(new[] { new Token(TokenType.Terminate, "EOF") }).ToArray();
+            return codes.Select(c => GetToken(c)).Concat(new[] { new Token(TokenType.Terminate, "EOF") }).ToList();
         }
 
         public Token GetToken(string str)
         {
-            return null;
+            switch (str)
+            {
+                case "+":
+                    return new Token(TokenType.Plus, str);
+                case "-":
+                    return new Token(TokenType.Minus, str);
+                case "*":
+                    return new Token(TokenType.Star, str);
+                case "/":
+                    return new Token(TokenType.Slash, str);
+                case "=":
+                    return new Token(TokenType.Equal, str);
+                case ";":
+                    return new Token(TokenType.End, str);
+                case "{":
+                    return new Token(TokenType.LeftBlock, str);
+                case "}":
+                    return new Token(TokenType.RightBlock, str);
+                case "let":
+                    return new Token(TokenType.Let, str);
+                case "print":
+                    return new Token(TokenType.Print, str);
+                case "function":
+                    return new Token(TokenType.Function, str);
+                case "return":
+                    return new Token(TokenType.Return, str);
+                default:
+                    float num;
+                    if (float.TryParse(str, out num)) return new Token(TokenType.Number, str);
+                    return new Token(TokenType.Symbol, str);
+            }
         }
 
     }
